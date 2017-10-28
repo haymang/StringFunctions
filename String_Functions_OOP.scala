61dd307cdea90807b04e82070469b57db734ff95
Haymang Ahuja <haymangahuja@outlook.com>
2017-10-29 02:54:15 +0530

    OOP Final_Version_0.3

--- String_Functions_OOP.scala
+++ String_Functions_OOP.scala
@@ -18,22 +18,22 @@ object String_Functions {
 				4. Check whether the string is a Palindrome or not
 				5. Exit""");
 
-		println("Enter your choice");
-		var userChoice:Int = scala.io.StdIn.readInt();
-
-		val call = userChoice match{
-		case 1 => println("Lowercase: " +obj.LowerCase());
-		case 2 => println("Uppercase: " +obj.UpperCase());
-		case 3 => println("Reverse String: " +obj.ReverseString());
-		case 4 => println("Is Palindrome: " +obj.PalindromeCheck());
-		case 5 => println("Good Choice, Bye"); sys.exit();
-		case _ => {
-			println("Not a valid choice!");
-			while(userChoice<1 || userChoice>5){
+		breakable {
+			while(true){
 				println("Enter your choice");
 				var userChoice:Int = scala.io.StdIn.readInt();
+
+				val call = userChoice match{
+				case 1 => println("Lowercase: " +obj.LowerCase());
+				case 2 => println("Uppercase: " +obj.UpperCase());
+				case 3 => println("Reverse String: " +obj.ReverseString());
+				case 4 => println("Is Palindrome: " +obj.PalindromeCheck());
+				case 5 => println("Good Choice, Bye"); sys.exit();
+				case _ => println("Invalid Input,Enter a valid option");
+
+
+				}
 			}
-		}
 		}
 	}
 }
